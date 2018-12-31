a08738f128ec4411e2414ecca092f313eda94205
David Hickey <david.hickey15@imperial.ac.uk>
2018-12-31 04:37:04 +0000

    Changed metadata

--- explainxkcd_button.js
+++ explainxkcd_button.js
@@ -1,16 +1,13 @@
 // ==UserScript==
 // @name        ExplainXKCD Button
-// @namespace   b945ae7b-7221-44c6-80e4-675306c15176
+// @description Replace the XKCD store button with a link to ExplainXKCD
+// @namespace   https://github.com/David-Hickey/ExplainXKCD-Button
 // @include     https://xkcd.com/*
 // @include     https://www.xkcd.com/*
-// @version     1
+// @version     1.1
 // @grant       none
 // @require http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
-
-/*
- * Userscript that turns the XKCD store button into a link to explainxkcd.
- */
 
 var storeButton = $("div#topLeft li > a:contains('Store')");
 
