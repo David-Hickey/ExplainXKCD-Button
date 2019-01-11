bedad8130d1fd049be9482a21668ffdd56750d69
David Hickey <44777933+David-Hickey@users.noreply.github.com>
2019-01-11 21:41:34 +0000

    Added configuration options

--- explainxkcd_button.js
+++ explainxkcd_button.js
@@ -8,15 +8,18 @@
 // @grant       none
 // ==/UserScript==
 
+// Feel free to modify these to change the behaviour.
+let BUTTON_TO_REPLACE = 3; // Takes values from 0-4.
+let OPEN_IN_NEW_TAB = true; // true or false.
+
 // This regex extracts the number from the URL, and only returns a
 // match for valid pages on the site.
 let extractNumber = /^https?:\/\/(?:www\.)?xkcd.com\/(?:(\d*)\/?)?$/i;
 let match = extractNumber.exec(window.location);
 
 if (match) {
-  // Select the fourth button in the top-left div, which is the
-  // Store button.
-  let storeButton = document.querySelectorAll("div#topLeft li")[3];
+  // Select the Nth button in the top-left div.
+  let storeButton = document.querySelectorAll("div#topLeft li")[BUTTON_TO_REPLACE];
   let buttonHref = storeButton.firstChild;
 
   // Extract the comic number from the match. If no number in the URL
@@ -27,6 +30,12 @@ if (match) {
     buttonHref.href = "https://explainxkcd.com/" + comicNumber + "/";
   } else {
     buttonHref.href = "https://explainxkcd.com/";
+  }
+  
+  if (OPEN_IN_NEW_TAB) {
+    buttonHref.target = "_blank";
+  } else {
+    buttonHref.target = "_self";
   }
   
   buttonHref.text = "Explain";
